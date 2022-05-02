962079e5771d892943eaff91c4465d84408bf16e
AlexeevSasha <work.alexeevs@gmail.com>
2022-05-02 21:37:51 +0300

    add new post

---
+++ src/pages/AddNewPost.tsx
@@ -0,0 +1,12 @@
+import {FC} from "react";
+import {AddPostForm} from "../components/AddPostForm/AddPostForm";
+
+
+export const AddNewPost: FC = () => {
+    return (
+        <div style={{marginTop: 20}}>
+            <AddPostForm/>
+        </div>
+    )
+}
+
