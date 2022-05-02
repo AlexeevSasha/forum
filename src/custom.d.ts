99ffaadd0ec46aa76f14193a36c1d9e1b434865b
AlexeevSasha <work.alexeevs@gmail.com>
2022-05-02 18:13:29 +0300

    module png, svg

---
+++ src/custom.d.ts
@@ -0,0 +1,19 @@
+declare module '*.svg' {
+    import * as React from 'react';
+
+    export const ReactComponent: React.FunctionComponent<React.SVGProps<
+        SVGSVGElement
+    > & { title?: string }>;
+
+    const src: string;
+    export default src;
+}
+declare module '*.png' {
+    const value: import('react').ImageSourcePropType;
+    export default value;
+}
+
+declare module '*.pdf' {
+    const value: import('react').ImageSourcePropType;
+    export default value;
+}
